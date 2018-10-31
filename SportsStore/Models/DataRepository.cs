3a57294c66c382b9860520678ad6207fc16089cd
Jeong H Park <hello@jeonghyuk.info>
2018-10-31 09:54:37 -0400

    Completed. (p.120 of 656) -Modifying Objects -Deleting Data

--- SportsStore/Models/DataRepository.cs
+++ SportsStore/Models/DataRepository.cs
@@ -27,9 +27,47 @@ namespace SportsStore.Models
 
         #region Methods
 
+        public Product GetProduct(long key) => context.Products.Find(key);
+
         public void AddProduct(Product product)
         {
             context.Products.Add(product);
+            context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            Product p = GetProduct(product.Id);
+
+            p.Name = product.Name;
+            p.Category = product.Category;
+            p.PurchasePrice = product.PurchasePrice;
+            p.RetailPrice = product.RetailPrice;
+
+            //context.Products.Update(product);
+            context.SaveChanges();
+        }
+
+        public void UpdateAll(Product[] products)
+        {
+            Dictionary<long, Product> data = products.ToDictionary(p => p.Id);
+            IEnumerable<Product> baseline = context.Products.Where(p => data.Keys.Contains(p.Id));
+
+            foreach(Product databaseProduct in baseline)
+            {
+                Product requestProduct = data[databaseProduct.Id];
+                databaseProduct.Name = requestProduct.Name;
+                databaseProduct.Category = requestProduct.Category;
+                databaseProduct.PurchasePrice = requestProduct.PurchasePrice;
+                databaseProduct.RetailPrice = requestProduct.RetailPrice;
+            }
+            
+            context.SaveChanges();
+        }
+
+        public void Delete(Product product)
+        {
+            context.Products.Remove(product);
             context.SaveChanges();
         }
 
