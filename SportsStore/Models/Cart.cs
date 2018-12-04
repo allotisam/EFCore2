47a13e6ce797b308bda845be00fa334c6833d2f7
Jeong H Park <hello@jeonghyuk.info>
2018-12-04 10:25:16 -0500

    Completed p.199 of 656 -Adding the Shopping Cart --Enabling Persistent Session DAta --Creating the Cart Model class --Creating the Controller and Views --Testing the Ordering Process

---
+++ SportsStore/Models/Cart.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class Cart
+    {
+        private List<OrderLine> selections = new List<OrderLine>();
+
+        public IEnumerable<OrderLine> Selections { get => selections; }
+
+        public Cart AddItem(Product p, int quantity)
+        {
+            OrderLine line = selections.Where(s => s.ProductId == p.Id).FirstOrDefault();
+
+            if (line != null)
+            {
+                line.Quantity += quantity;
+            }
+            else
+            {
+                selections.Add(new OrderLine
+                {
+                    ProductId = p.Id,
+                    Product = p,
+                    Quantity = quantity                    
+                });
+            }
+
+            return this;
+        }
+
+        public Cart RemoveItem(long productId)
+        {
+            selections.RemoveAll(s => s.Product.Id == productId);
+
+            return this;
+        }
+
+        public void Clear() => selections.Clear();
+    }
+}
