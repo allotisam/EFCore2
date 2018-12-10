6662e1573fab9acc2ea583a3f46dd3b14687cd62
Jeong H Park <hello@jeonghyuk.info>
2018-12-10 09:50:31 -0500

    Completed p.217 of 656 -Creating a Web Service --Creating the Repo --Creating the API Controller --Testing the WebService --Projectinig a Result to Exclude Null Navigation Properties --Including Related Data (Avoiding Circular References) --querying Multiple Objects -Completing the Web Service --Updating the Controller

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace SportsStore
 {
@@ -22,10 +23,11 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(opts => opts.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize);
             services.AddTransient<IRepository, DataRepository>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<IOrdersRepository, OrdersRepository>();
+            services.AddTransient<IWebServiceRepository, WebServiceRepository>();
 
             string conString = Configuration["ConnectionStrings:DefaultConnection"];
             services.AddDbContext<DataContext>(options => options.UseSqlServer(conString));
